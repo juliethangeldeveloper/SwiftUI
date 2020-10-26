e5119df30cb6ccdf1443e39530f9fc204f43788b
Julieth Angel <45715217+juliethangeldeveloper@users.noreply.github.com>
2020-10-25 22:27:16 -0400

    Project created with app icon added

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Julieth Angel on 10/25/20.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
