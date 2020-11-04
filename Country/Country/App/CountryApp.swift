6008bb905b5d5f3e4dd3ca40693c1e3e01e19fb1
Julieth Angel <45715217+juliethangeldeveloper@users.noreply.github.com>
2020-11-03 21:31:42 -0500

    New Projetc Added

---
+++ Country/Country/App/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Julieth Angel on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Country/Country/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Country
+//
+//  Created by Julieth Angel on 11/3/20.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
